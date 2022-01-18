ebc644b9a97b49da4d90e58baf0363e0ba53df91
romerodevelopments <gandresrp@gmail.com>
2022-01-18 16:12:46 -0300

    se corrige bug en buscador

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,4 +1,8 @@
+/* https://sequelize.org/v5/manual/models-usage.html */
+
 var Product = require('../models').product;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 module.exports = {
   //A traves de la funcion findAll del modelo se traen todos los productos y envia una respuesta de los mismos en formato json
@@ -10,7 +14,9 @@ module.exports = {
     res.header("Access-Control-Allow-Origin","*");
     Product.findAll({
       where: {
-        name: req.params.wildcard
+        name: {
+          [Op.like]: `%${req.params.wildcard}%`
+        }
       }
     }).then(result => res.json(result));
   }
