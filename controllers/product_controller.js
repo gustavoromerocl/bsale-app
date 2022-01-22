31324d18416acab6b723566ec56ff3771558b394
romerodevelopments <gandresrp@gmail.com>
2022-01-22 17:57:49 -0300

    pagination feat - se configura paginacion en el servidor con sequelize

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -8,7 +8,31 @@ module.exports = {
   //A traves de la funcion findAll del modelo se traen todos los productos y envia una respuesta de los mismos en formato json
   index: function(req, res){
     res.header("Access-Control-Allow-Origin","*");
-    Product.findAll().then(products => res.json(products));
+    /**
+     * En las siguientes líneas de codigo se valida que la infromación recibida desde los query params sea un valor number, ya que 
+     * si es otro tipo de dato genera inconsistencias en la query que realiza sequelize
+     */
+    const pageAsNumber = Number.parseInt(req.query.page);
+    const sizeAsNumber = Number.parseInt(req.query.size);
+
+    //valores por defecto
+    let page = 0;
+    let size = 8;
+
+    if(!Number.isNaN(pageAsNumber) && pageAsNumber > 0) page = pageAsNumber;
+    
+
+    if(!Number.isNaN(sizeAsNumber)&& sizeAsNumber > 0 && sizeAsNumber < 10) size = sizeAsNumber
+    
+    Product.findAndCountAll({
+      limit: size,
+      offset: page * size
+    })
+    .then(products => res.json({
+      content: products.rows,
+      totalPages: Math.ceil(products.count / size)
+    }));
+    
   },
   //Filtro de busqueda de productos por el parametro ingresado en la barra de busqueda
   filter: function(req, res){
